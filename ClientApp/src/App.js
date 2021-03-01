58d621178e54900a98aced64dbfaf2bc167302bd
Lubomir Sokolovsky <lubomir.sokolovsky@tietoevry.com>
2021-03-01 09:35:40 +0100

    Adding default redirect.

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router';
+import { Route, Switch } from 'react-router';
 import { Layout } from './components/Layout';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import { RedirectRoot } from "./components/RootRedirect";
 
 import './custom.css'
 
@@ -12,8 +13,11 @@ export default class App extends Component {
   render () {
     return (
       <Layout>
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
+        <Switch>
+            <Route path='/counter' component={Counter} />
+            <Route path='/fetch-data' component={FetchData} />
+            <Route path="/" component={RedirectRoot} />
+        </Switch>
       </Layout>
     );
   }
---
+++ ClientApp/src/components/RootRedirect.js
@@ -0,0 +1,8 @@
+﻿import React from 'react';
+
+export const RedirectRoot = () => {
+    const redirectUrl = `${window.location.protocol}//${window.location.host}/home`;
+    console.log(redirectUrl);
+    window.location.replace(redirectUrl);
+    return null;
+}
